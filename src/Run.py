0917d3d8b286a600a64622a53d56191b5dc399f6
Dragan Stojanovic <drakche.69@gmail.com>
2011-07-02 18:56:27 +0200

    Added a database

--- src/Run.py
+++ src/Run.py
@@ -5,24 +5,34 @@ Created on Jun 28, 2011
 '''
 from GUI_V2 import MainFrame
 import wx
-
+import sqlite3
 from wx._misc import DateTime
 
 class Exam(object):    
-    def __init__(self, subject='', grade=0, date=DateTime.Now()):
+    def __init__(self, subject='', grade=0, esbp=0):
         self.Subject = subject
         self.Grade = grade
-        self.Date = date
+        self.ESBP = esbp
         
     def __repr__(self, *args, **kwargs):
-        return '{0} - {1} ({2})'.format(self.Subject,str(self.Grade),self.Date.Format('%d-%m-%Y'))
+        return '{0} - {1} ({2})'.format(self.Subject,str(self.Grade),str(self.ESBP))
+ 
+class DBBroker():
+    @staticmethod
+    def Insert(tupleList):
+        conn = sqlite3.connect('Calc.db3')
+        
+        c = conn.cursor()
+        for tuple in tupleList:
+            c.execute('insert into Exam(Name, Grade, ESBP, Year) values(?,?,?,?)',tuple)        
+        conn.commit()
+        c.close()
+        
         
 class GUIOverride(MainFrame):
     def __init__(self, parent):
         MainFrame.__init__(self, parent)
-        
-        
-        
+                    
         self.activeYear = self.tabNotebook.GetSelection()
         self.selectedItem = -1
         self.list = [[],[],[],[]]
@@ -37,6 +47,9 @@ class GUIOverride(MainFrame):
         
         self.ClearFields()
         
+    def mItemExit_OnMenuSelection(self, event):
+        self.Destroy()
+        
     def ClearFields(self):
         self.txtSubject.Value = ''
         self.datDatum.Value = DateTime.Now()
@@ -45,17 +58,19 @@ class GUIOverride(MainFrame):
         self.btnDelete.Disable()
         
     def tabNotebookOnNotebookPageChanged(self, event):
-        self.activeYear = self.tabNotebook.GetSelection()
+        self.activeYear = event.GetSelection()
         self.UnselectList()
         event.Skip()
+    
+        
     
     def btnAdd_OnButtonClick(self, event):        
         ex = Exam()
         ex.Subject = self.txtSubject.Value
         ex.Grade = self.spinGrade.Value
-        ex.Date = self.datDatum.Value
+        ex.ESBP = 0
         
-        
+        DBBroker.Insert([(ex.Subject,ex.Grade,ex.ESBP,self.activeYear+1)])
         
         exist = False
         
@@ -84,9 +99,14 @@ class GUIOverride(MainFrame):
         self.btnDelete.Enable()
         
     def btnDelete_OnButtonClick(self, event):
-        self.list[self.activeYear].pop(self.selectedItem)
-        self.tabNotebook.GetPage(self.activeYear).GetChildren()[0].Delete(self.selectedItem)
-        self.ClearFields()
+        res = wx.MessageDialog(None,'Da li zelite da obrisete ispit?',
+                  'Info',   
+                  wx.YES_NO|wx.NO_DEFAULT|wx.ICON_QUESTION).ShowModal()
+        if res == wx.ID_YES:
+            self.list[self.activeYear].pop(self.selectedItem)
+            self.tabNotebook.GetPage(self.activeYear).GetChildren()[0].Delete(self.selectedItem)
+            self.ClearFields()
+        
         
         
 if __name__ == '__main__':
